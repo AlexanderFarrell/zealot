de8c9a3a9eacbc18165e0c7d3e0db0dbb4730aad
Alexander Farrell <info@alexanderfarrell.com>
2025-11-29 15:47:18 +0000

    Added event system in Go.

---
+++ zealotd/web/events.go
@@ -0,0 +1,27 @@
+package web
+
+import "fmt"
+
+var (
+	events = map[string][]func([]string){}
+)
+
+func EventOn(event string, on func([]string)) {
+	events[event] = append(events[event], on)
+}
+
+func EventOff(event string, on func([]string)) {
+	handlers := events[event]
+	for i, h := range handlers {
+		if fmt.Sprintf("%p", h) == fmt.Sprintf("%p", on) {
+			events[event] = append(handlers[:i], handlers[i+1:]...)
+			return
+		}
+	}
+}
+
+func Emit(event string, args []string) {
+	for _, fn := range events[event] {
+		fn(args)
+	}
+}
