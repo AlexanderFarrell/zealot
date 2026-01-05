f4b487edc36d7bc5b3599bccedcd6bed611d0724
AlexanderFarrell <courses@alexanderfarrell.com>
2026-01-05 23:07:34 +0000

    Hotfix, removed table_builder code.

--- client/src/components/table_builder.ts
+++
@@ -1,9 +0,0 @@
-function build_table<T>(headers: string[], items: T[], 
-	build_row: (t: T) => HTMLElement[]
-) 
-{
-	let table = document.createElement('table');
-	table.innerHTML = 
-
-	return table;
-}
